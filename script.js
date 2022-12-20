74f87bcde38aa3c07625579744344d035e2754c8
Panihida <114984989+idanmz@users.noreply.github.com>
2022-12-20 09:20:19 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+let film = [
+    ['The Godfather' , 'Casablanka' , 'Gladiator' , 'Titanic' , 'Rocky' ] ,
+    ['Patton' , 'The Pianist' , 'Z' , 'The Green Mile' , 'Home Alone'],
+    ['Avatar' , 'Harry Potter' , 'Venom' ]
+]
+document.write(film[0][1][2]+film[0][1][1]+film[0][2][4]+film[0][2][3]+film[0][1][1])
+console.log(film[1][0][5]+film[0][1][1]+film[1][4][2]+film[0][1][1]+film[1][2][0]+film[1][0][4]+film[2][2][0]+film[0][1][1]+" "+film[0][1][2]+film[0][1][1]+film[0][2][4]+film[0][2][3]+film[0][1][1])
